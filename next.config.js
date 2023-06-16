21f8c568c3ddfe8be26ba145f56808cf587c8f59
Muhammad Samir <79057248+MuhammadSamir1212@users.noreply.github.com>
2023-06-16 11:28:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,15 @@ const nextConfig = {
   env: {
     API_URL: "https://nextjs-13-full-stack-ecommerce.vercel.app",
     MONGODB_URI:
-      "mongodb+srv://muhammad:H6lXIHKl53hR0p9K@cluster0.k6foovo.mongodb.net/next13-ecommarce?retryWrites=true&w=majority",
-    NEXTAUTH_SECRET: "msaprogramming",
+      "",
+    NEXTAUTH_SECRET: "",
     NEXTAUTH_URL: "https://nextjs-13-full-stack-ecommerce.vercel.app",
     CLOUDINARY_URL:
-      "https://api.cloudinary.com/v1_1/msa-programing/image/upload",
-    CLOUDINARY_PRESET: "msa-programming",
-    CLOUDINARY_NAME: "msa-programing",
+      "",
+    CLOUDINARY_PRESET: "",
+    CLOUDINARY_NAME: "",
     PAYPAL_CLIENT_ID:
-      "AW7fuiuwWy6K9w5AkxTUv2I1Y5XAwO7aFwlqJPxyw0Mhdc2eHI3DqmNZcqw9WWjbp-t-SOLjyer-3wmf",
+      "",
   },
   images: {
     remotePatterns: [
